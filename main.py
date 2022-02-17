807c20d94d228c15b849cf27643f1899b6a32cf2
Silver911r <30500702+Silver911r@users.noreply.github.com>
2022-02-17 14:45:43 -0500

    moved engine into loop

--- main.py
+++ main.py
@@ -42,6 +42,5 @@ for pagenum in range(startpage, numofPages):
     lines = page.splitlines()
     for line in lines:
         engine.say(line)
-
-# run the engine
-engine.runAndWait()
+    # run the engine
+    engine.runAndWait()
